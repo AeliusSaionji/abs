71640c8e32f2ac11eb4cbf6889a99cc974a5551d
Link Satonaka <link-satonaka@outlook.com>
2015-05-16 23:44:03 -0400

    ask about dvtm

--- install-all.sh
+++ install-all.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-yaourt -U ./dmenu-personalized/dmenu-personalized-4.5.13.5ed5e90-1-x86_64.pkg.tar.xz ./dvtm/dvtm-0.14-1-x86_64.pkg.tar.xz ./dwm-personalized/dwm-personalized-6.0.43.g14343e6-1-x86_64.pkg.tar.xz ./slock-class/slock-class-1.2-1-x86_64.pkg.tar.xz ./st-personalized/st-personalized-0.5-2-x86_64.pkg.tar.xz ./xombrero/xombrero3-1.6.4-1-x86_64.pkg.tar.xz
+yaourt -U ./dmenu-personalized/dmenu-personalized-4.5.13.5ed5e90-1-x86_64.pkg.tar.xz ./dwm-personalized/dwm-personalized-6.0.43.g14343e6-1-x86_64.pkg.tar.xz ./slock-class/slock-class-1.2-1-x86_64.pkg.tar.xz ./st-personalized/st-personalized-0.5-2-x86_64.pkg.tar.xz ./xombrero/xombrero3-1.6.4-1-x86_64.pkg.tar.xz
+echo install dvtm with mod F13?
+yaourt -U ./dvtm/dvtm-0.14-1-x86_64.pkg.tar.xz 
