6bdf8e46321748abdbae278e6c9884d00d7d4f7a
Link Satonaka <link-satonaka@outlook.com>
2015-05-16 23:52:19 -0400

    more visibility for the question. too lazy to add a real prompt

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 yaourt -U ./dmenu-personalized/dmenu-personalized-4.5.13.5ed5e90-1-x86_64.pkg.tar.xz ./dwm-personalized/dwm-personalized-6.0.43.g14343e6-1-x86_64.pkg.tar.xz ./slock-class/slock-class-1.2-1-x86_64.pkg.tar.xz ./st-personalized/st-personalized-0.5-2-x86_64.pkg.tar.xz ./xombrero/xombrero3-1.6.4-1-x86_64.pkg.tar.xz
-echo install dvtm with mod F13?
+color='\033[1;31m'
+NC='\033[0m' # No Color
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+color='\033[0;31m'
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+color='\033[1;35m'
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+color='\033[0;35m'
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+color='\033[1;34m'
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+color='\033[0;34m'
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
+printf "${color}install dvtm with mod F13?\n${NC}"
 yaourt -U ./dvtm/dvtm-0.14-1-x86_64.pkg.tar.xz 
