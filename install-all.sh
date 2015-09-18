52ddb65f5e914bdca93ea4cce1f3da5a044660e2
Link Satonaka <link.satonaka@outlook.com>
2015-09-18 01:44:55 -0400

    kill this script until we fix it

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# rework this using makepkg -fic
+exit
 sudo pacman -U ./dmenu-personalized/dmenu-personalized-4.5.13.5ed5e90-1-x86_64.pkg.tar.xz ./dwm-personalized/dwm-personalized-6.0.43.g14343e6-1-x86_64.pkg.tar.xz ./slock-class/slock-class-1.2-1-x86_64.pkg.tar.xz ./st-personalized/st-personalized-0.5-2-x86_64.pkg.tar.xz ./xombrero/xombrero3-1.6.4-1-x86_64.pkg.tar.xz
 NC='\033[0m' # No Color
 color='\033[1;35m'
