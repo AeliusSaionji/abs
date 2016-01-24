8f271c369a4e7487d74f9778dcb239781c1eadea
Link Satonaka <link.satonaka@outlook.com>
2016-01-24 05:37:26 -0500

    font antialias and autohint

--- st-personalized/config.h
+++ st-personalized/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "inconsolata:pixelsize=12";
+static char font[] = "inconsolata:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
