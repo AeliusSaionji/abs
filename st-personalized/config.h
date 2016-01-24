1886b4137c62d382b7c91f8e4524203058e30847
Link Satonaka <link.satonaka@outlook.com>
2016-01-24 15:35:25 -0500

    autohint sucks

--- st-personalized/config.h
+++ st-personalized/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "inconsolata:pixelsize=12:antialias=true:autohint=true";
+static char font[] = "inconsolata:pixelsize=12:antialias=true:autohint=false";
 static int borderpx = 0;
 
 /*
